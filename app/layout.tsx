a498ca47ccdefdca745d991db792e9153ae792ba
Patrice <abellepatrice@gmail.com>
2025-10-18 17:56:17 +0300

    Addition of analytics and speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "../components/Navbar"; 
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "AbelleGroup",
@@ -16,10 +18,39 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar/>
+        <Navbar />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
+
+// import type { Metadata } from "next";
+// import "./globals.css";
+// import Navbar from "../components/Navbar"; 
+// import Footer from "../components/Footer";
+// import { Analytics } from "@vercel/analytics/next"
+
+// export const metadata: Metadata = {
+//   title: "AbelleGroup",
+//   description: "AbelleGroup Portfolio and Project Hub",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <Navbar/>
+//         {children}
+//         <Footer/>
+//         </body>
+//     </html>
+//   );
+// }
